963209cb1e45d631aeac63de43ec377d2f2ab5d6
VALDEMIRf <valdemirframos@gmail.com>
2023-10-12 22:07:27 -0300

    primeiro commit

---
+++ PDV_FRENTE_CAIXA/mdlConexao.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data.SqlClient
+Module mdlConexao
+    ' Public con As New SqlConnection("Data Source=DESKTOP-9357516;Initial Catalog=sistemavb;Integrated Security=True")
+    Public con As New SqlConnection("Data Source=DESKTOP-R5VHKNO\SQLEXPRESS;Initial Catalog=frente_caixa;Integrated Security=True")
+
+    Sub abrir()
+        If con.State = 0 Then
+            con.Open()
+        End If
+    End Sub
+
+    Sub fechar()
+        If con.State = 1 Then
+            con.Close()
+        End If
+    End Sub
+
+    Public usuarioNome As String
+
+    Public idempresa As String
+
+End Module
---
+++ PDV_FRENTE_CAIXA/mdlDiversos.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Text
+Module mdlDiversos
+    Public Function UTF8_to_ISO(texto As String) As String
+        'Dim isoEncoding = Encoding.GetEncoding("ISO-8859-8")
+        Dim isoEncoding = Encoding.GetEncoding("Windows-1252")
+        Dim utfEncoding = Encoding.UTF8
+
+        Dim bytesIso As Byte() = utfEncoding.GetBytes(texto)
+        Dim bytesUtf As Byte() = Encoding.Convert(utfEncoding, isoEncoding, bytesIso)
+
+        Dim textoISO = utfEncoding.GetString(bytesUtf)
+
+        UTF8_to_ISO = textoISO
+    End Function
+
+    Public Sub permiteSoNumeros(sender As Object, e As KeyPressEventArgs)
+        If Not IsNumeric(e.KeyChar) And Asc(e.KeyChar) <> 8 Then
+            e.Handled = True
+        End If
+    End Sub
+
+End Module
